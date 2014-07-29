da743922cf0c00494b34b6a8f3f47b506196f912
Justin <justincyork@gmail.com>
2014-07-28 22:12:08 -0500

    Style list; update fs-traversal
    Need to add person card popup and relationship view popup

---
+++ includes/fs-traversal-relationship-display-0.2.0.js
@@ -0,0 +1,155 @@
+(function(){
+
+  // Width of a person box, in px
+  var boxWidth = 180;
+
+  window.FSTraversalRelationshipDisplay = function(traversalPath){
+  
+    // Modify path data structure
+    var path = [];
+    for(var i = 0; i < traversalPath.length; i ++){
+      path.push({
+        person: traversalPath[i].person,
+        relationship: traversalPath[i].rel,
+        corner: '',
+        lines: []
+      });
+    }
+    
+    // Calculate height, corner, and lines
+    var prevHeight = path[0].height = 0;
+    var prevChange = 0;
+    var minHeight = 0;
+    for(var i = 1; i < path.length; i++){
+      
+      // Calculate height
+      var change = 0;
+      switch(path[i].relationship){
+        case 'child':
+          change = -1;
+          break;
+        case 'mother':
+        case 'father':
+          change = 1;
+          break;
+      }
+      prevHeight = path[i].height = prevHeight + change;
+      minHeight = Math.min(prevHeight, minHeight);
+      
+      // Calculate corner value for previous person
+      if(change === 1 && prevChange === -1){
+        path[i-1].corner = 'bottom';
+      } else if(prevChange === 1 && change === -1){
+        path[i-1].corner = 'top';
+      }
+      
+      // Calculate lines
+      if(path[i].relationship === 'child'){
+        path[i].lines.push('top');
+        path[i-1].lines.push('bottom');
+      } else if(path[i].relationship === 'mother' || path[i].relationship === 'father'){
+        path[i].lines.push('bottom');
+        path[i-1].lines.push('top');
+      } else if(path[i].relationship === 'spouse'){
+        path[i].lines.push('left');
+      }
+      if(path[i-1].corner === 'top'){
+        path[i-2].lines.push('top-right');
+        path[i].lines.push('top-left');
+      } else if(path[i-1].corner === 'bottom'){
+        path[i-1].lines.push('top-full');
+      }
+      
+      prevChange = change;
+    }
+    
+    // Adjust heights so that the minimum is 0
+    if(minHeight < 0){
+      var change = Math.abs(minHeight);
+      for(var i = 0; i < path.length; i++){
+        path[i].height += change;
+      }
+    }
+    
+    // Calculate left
+    var prevLeft = path[0].left = 0;
+    for(var i = 1; i < path.length; i++){
+      var change = 0;
+      if(path[i-1].corner || path[i].corner){
+        change = .5;
+      }
+      else if(path[i].relationship === 'spouse'){
+        change = 1;
+      }
+      prevLeft = path[i].left = prevLeft + change;
+    }
+    
+    // Group people into rows based on height
+    var rowsMap = {};
+    for(var i = 0; i < path.length; i++){
+      var rowNumber = path[i].height;
+      if(!rowsMap[rowNumber]){
+        rowsMap[rowNumber] = [];
+      }
+      rowsMap[rowNumber].push(path[i]);
+    }
+    
+    // Sort rows in reverse according height
+    var rows = [];
+    for(var n in rowsMap){
+      rows.push({
+        number: parseInt(n),
+        // Sort people in the row too according to left position
+        people: rowsMap[n].sort(function(a, b){
+          return a.left - b.left;
+        })
+      });
+    }
+    rows.sort(function(a, b){
+      return b.number - a.number;
+    });
+    
+    // Output rows; use inline css positioning for now    
+    var html = '',
+        maxRight = 180;
+    for(var i = 0; i < rows.length; i++){
+      html += '<div class="fst-row">';
+      var prevLeft = 0;
+      for(var j = 0; j < rows[i].people.length; j++){
+        var person = rows[i].people[j],
+            myLeft = person.left * boxWidth,
+            cornerClass = person.corner ? ' fst-corner-' + person.corner : '';
+        html += '<div class="fst-person'+cornerClass+'" style="margin-left:'+(myLeft - prevLeft)+'px">';
+        html += '<div class="fst-name">'+person.person.display.name+'</div>';
+        html += '<div class="fst-role">'+getRelationshipString(person)+'</div>';
+        for(var k = 0; k < person.lines.length; k++){
+          html += '<div class="fst-line-'+person.lines[k]+'"></div>';
+        }
+        html += '</div>'; // end person
+        prevLeft = myLeft + 180;
+      }
+      maxRight = Math.max(maxRight, prevLeft);
+      html += '</div>';
+    }
+    
+    return '<div class="fst-relationship-display" style="width:'+maxRight+'px">' + html + '</div>';
+  };
+  
+  function getRelationshipString(person){
+    var rel = person.relationship;
+    if(rel === ''){
+      return 'you';
+    }
+    else if(rel === 'child'){
+      if(person.person.gender.type === 'http://gedcomx.org/Male'){
+        return 'son';
+      } else if(person.person.gender.type === 'http://gedcomx.org/Female'){
+        return 'daughter';
+      } else {
+        return 'child';
+      }
+    }
+    return rel;
+  };
+
+}());
--- js/index.js
+++ js/index.js
@@ -4,19 +4,43 @@
 // but it's nice for now. Need to keep watch for the possibility that they
 // stop checking for this on 404 pages.
 
+/**
+
+ TODO:
+ 
+ 1. Better display
+    * Loading indication
+    * "Show me more" button
+    * Better styling of results
+    * Change HTML title of the page
+ 2. Figure out why API returns matches when tree doesn't show them
+
+**/
+
 var queue = async.queue(queueWorker, 5),
     step = max = 10,
     count = 0,
     token,
     traversal,
-    list = $('<div id="hl-hints-list">');
+    list = $('<div id="hl-hints-list">'),
+    hintRowTemplate = '';
+    
+document.title = 'Record Hints List | FamilySearch.org';
+
+$.get(chrome.extension.getURL('templates/hint-row.html')).done(function(html){
+  hintRowTemplate = html;
+  Mustache.parse(hintRowTemplate);
+});
 
 $(document).ready(function(){
 
-  $('#subject').html(list);
+  $('.app-home').html(list).append('<img src="'+chrome.extension.getURL('img/loader-big-333333-00000.gif')+'">');
 
   // Get session
   token = cookiesUtil.getItem('fssessionid');
+  
+  // Get start ID
+  var start = location.pathname.substring(1).split('/')[1];
   
   // Setup SDK
   FamilySearch.init({
@@ -29,13 +53,13 @@ $(document).ready(function(){
   
   // Setup traversal
   traversal = FSTraversal(FamilySearch)
-    .order('ancestry')
+    .order('wrd-far')
     .person(function(person){
       if(!person.living){
         queue.push(person);
       }
     })
-    .traverse();
+    .traverse(start);
 });
 
 /**
@@ -45,9 +69,19 @@ function queueWorker(person, callback){
   if(count++ === max){
     traversal.pause();
   }
-  $.getJSON('https://familysearch.org/tree-data/record-matches/' + person.id + '?fsSession=' + token).done(function(response){
+  $.getJSON('https://familysearch.org/tree-data/record-matches/' + person.id).done(function(response){
     if(response && response.status === 'OK' && response.data.matches.length > 0){
-      list.append('<p><a href="https://familysearch.org/tree/#view=allMatchingRecords&person='+person.id+'">'+person.display.name+'</a></p>')
+      //list.append('<p><a href="https://familysearch.org/tree/#view=allMatchingRecords&person='+person.id+'">'+person.display.name+'</a></p>')
+      var row = Mustache.render(hintRowTemplate, {
+        id: person.id,
+        name: person.display.name,
+        relationship: traversal.relationshipTo(person.id),
+        count: response.data.matches.length
+      });
+      list.append(row);
+      $('.hl-relationship a', row).click(function(){
+        // TODO: popup
+      });
     } else {
       count--;
       if(count < max){
